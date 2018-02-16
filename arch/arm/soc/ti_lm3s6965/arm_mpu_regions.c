cdf6f65918c776d21529967365297cdc08f79541
Adithya Baglody <adithya.nagaraj.baglody@intel.com>
2018-02-16 14:06:33 +0530

    wip: qemu cortex m3
    Signed-off-by: Adithya Baglody <adithya.nagaraj.baglody@intel.com>

---
+++ arch/arm/soc/ti_lm3s6965/arm_mpu_regions.c
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2017 Linaro Limited.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <soc.h>
+#include <arch/arm/cortex_m/mpu/arm_mpu.h>
+
+
+/* SoC Private Peripheral Bus */
+#define PPB_BASE  0xE0000000
+
+static struct arm_mpu_region mpu_regions[] = {
+	/* Region 0 */
+	MPU_REGION_ENTRY("FLASH_0",
+			 CONFIG_FLASH_BASE_ADDRESS,
+			 REGION_FLASH_ATTR(REGION_256K)),
+	/* Region 1 */
+	MPU_REGION_ENTRY("RAM_0",
+			 CONFIG_SRAM_BASE_ADDRESS,
+			 REGION_RAM_ATTR(REGION_64K)),
+	/* Region 2 */
+	/* MPU_REGION_ENTRY("RAM_1", */
+	/*		 (CONFIG_SRAM_BASE_ADDRESS + REGION_SRAM_1_START), */
+	/*		 REGION_RAM_ATTR(REGION_SRAM_1_SIZE)), */
+	/* Region 3 */
+	/* MPU_REGION_ENTRY("PERIPHERAL_0", */
+	/*		 0x4000c000, */
+	/*		 REGION_IO_ATTR(REGION_32K)), */
+	/* /\* Region 4 *\/ */
+	/* MPU_REGION_ENTRY("PPB_0", */
+	/*		 PPB_BASE, */
+	/*		 REGION_PPB_ATTR(REGION_256M)), */
+
+};
+
+struct arm_mpu_config mpu_config = {
+	.num_regions = ARRAY_SIZE(mpu_regions),
+	.mpu_regions = mpu_regions,
+};
